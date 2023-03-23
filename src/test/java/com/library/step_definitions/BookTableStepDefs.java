355ae2854589b738b81168f2a9d283ea31d3098b
Emma Clark <clarkemma245@gmail.com>
2023-03-23 14:27:15 +0000

    Library Project

--- src/test/java/com/library/step_definitions/BookTableStepDefs.java
+++ src/test/java/com/library/step_definitions/BookTableStepDefs.java
@@ -16,8 +16,9 @@ public class BookTableStepDefs {
     @Then("book information must match the database for {string}")
     public void book_information_must_match_the_database_for(String bookName) {
         BrowserUtils.waitFor(3);
-       //get information from UI
-        String actualName = booksPage.bookName.getAttribute("value");
+
+        //get information from UI
+        String actualName=booksPage.bookName.getAttribute("value");
         String actualAuthor = booksPage.author.getAttribute("value");
         String actualISBN = booksPage.isbn.getAttribute("value");
         String actualYear = booksPage.year.getAttribute("value");
@@ -25,21 +26,21 @@ public class BookTableStepDefs {
 
         System.out.println("actualName = " + actualName);
         System.out.println("actualAuthor = " + actualAuthor);
-        //get information from database
 
-        String query = "select name, isbn,year,author,description from books\n" +
-                "where name = '"+bookName+"'";
+        //get info from dataBase
+        String query="select name,isbn,year,author,description from books\n" +
+                "where name='Chordeiles minor'";
 
         //since we are getting only one row, we will use getRowMap method
-        Map<String, Object> dbData = DBUtils.getRowMap(query);
+        Map<String,Object> dbData=DBUtils.getRowMap(query);
+        System.out.println("dbData.toString() = " + dbData.toString());
 
-        System.out.println(dbData.toString());
-
-        String expectedName = dbData.get("name").toString();
+        String expectedName=dbData.get("name").toString();
         String expectedISBN = dbData.get("isbn").toString();
         String expectedYear = dbData.get("year").toString();
         String expectedAuthor = dbData.get("author").toString();
         String expectedDesc = dbData.get("description").toString();
+
 
         //compare UI vs Database
         Assert.assertEquals("did not match name",expectedName,actualName);
